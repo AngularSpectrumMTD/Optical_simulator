1422a6d95abd38618b640134075c823ed7f6a36b
AngularSpectrum <integral11anddifferental20@yahoo.co.jp>
2020-08-01 01:03:43 +0900

    add error message for operator between wavefront and wavefront

--- cpp/WaveFront_Operator.cpp
+++ cpp/WaveFront_Operator.cpp
@@ -58,6 +58,12 @@ WaveFront& WaveFront::operator/=(double val)
 
 WaveFront& WaveFront::operator+=(const WaveFront& val)
 {
+	if (w_nx == val.w_nx && w_ny == val.w_ny && w_px == val.w_px && w_py == val.w_py && w_lambda == val.w_lambda)
+	{
+		printf(">>ERROR: operator +=: all parameters must be same\n");
+		printf(">>Process is terminated forcibly...\n");
+		exit(0);
+	}
 	int i, j;
 #pragma omp parallel for num_threads(omp_get_num_threads())
 	for (j = 0; j < w_ny; ++j)
@@ -72,6 +78,12 @@ WaveFront& WaveFront::operator+=(const WaveFront& val)
 
 WaveFront& WaveFront::operator-=(const WaveFront& val)
 {
+	if (w_nx == val.w_nx && w_ny == val.w_ny && w_px == val.w_px && w_py == val.w_py && w_lambda == val.w_lambda)
+	{
+		printf(">>ERROR: operator +=: all parameters must be same\n");
+		printf(">>Process is terminated forcibly...\n");
+		exit(0);
+	}
 	int i, j;
 #pragma omp parallel for num_threads(omp_get_num_threads())
 	for (j = 0; j < w_ny; ++j)
@@ -86,6 +98,12 @@ WaveFront& WaveFront::operator-=(const WaveFront& val)
 
 WaveFront& WaveFront::operator*=(const WaveFront& val)
 {
+	if (w_nx == val.w_nx && w_ny == val.w_ny && w_px == val.w_px && w_py == val.w_py && w_lambda == val.w_lambda)
+	{
+		printf(">>ERROR: operator +=: all parameters must be same\n");
+		printf(">>Process is terminated forcibly...\n");
+		exit(0);
+	}
 	int i, j;
 #pragma omp parallel for num_threads(omp_get_num_threads())
 	for (j = 0; j < w_ny; ++j)
@@ -100,6 +118,12 @@ WaveFront& WaveFront::operator*=(const WaveFront& val)
 
 WaveFront& WaveFront::operator/=(const WaveFront& val)
 {
+	if (w_nx == val.w_nx && w_ny == val.w_ny && w_px == val.w_px && w_py == val.w_py && w_lambda == val.w_lambda)
+	{
+		printf(">>ERROR: operator +=: all parameters must be same\n");
+		printf(">>Process is terminated forcibly...\n");
+		exit(0);
+	}
 	int i, j;
 #pragma omp parallel for num_threads(omp_get_num_threads())
 	for (j = 0; j < w_ny; ++j)
@@ -114,6 +138,12 @@ WaveFront& WaveFront::operator/=(const WaveFront& val)
 
 WaveFront& WaveFront::operator =(const WaveFront& val)
 {
+	if (w_nx == val.w_nx && w_ny == val.w_ny && w_px == val.w_px && w_py == val.w_py && w_lambda == val.w_lambda)
+	{
+		printf(">>ERROR: operator +=: all parameters must be same\n");
+		printf(">>Process is terminated forcibly...\n");
+		exit(0);
+	}
 	if (GetNx() == val.GetNx() && GetNy() == val.GetNy())
 	{
 		int i, j;
